102df1a08d2248cfa2c210c8b3a14821e7eed3cf
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-11-05 08:46:13 -0300

    Fechando o Swipeable via ref

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, ScrollView, Alert, Pressable } from 'react-native';
 import { HouseLine, Trash } from 'phosphor-react-native';
@@ -19,6 +19,8 @@ export function History() {
 
   const { goBack } = useNavigation();
 
+  const swipeableRefs = useRef<Swipeable[]>([]);
+
   async function fetchHistory() {
     const response = await historyGetAll();
     setHistory(response);
@@ -31,7 +33,9 @@ export function History() {
     fetchHistory();
   }
 
-  function handleRemove(id: string) {
+  function handleRemove(id: string, index: number) {
+    swipeableRefs.current?.[index].close();
+
     Alert.alert(
       'Remover',
       'Deseja remover esse registro?',
@@ -67,7 +71,7 @@ export function History() {
         showsVerticalScrollIndicator={false}
       >
         {
-          history.map((item) => (
+          history.map((item, index) => (
             <Animated.View
               key={item.id}
               entering={SlideInRight}
@@ -75,11 +79,16 @@ export function History() {
               layout={LinearTransition.springify()}
             >
               <Swipeable
+                ref={(ref) => {
+                  if(ref) {
+                    swipeableRefs.current.push(ref);
+                  }
+                }}
                 overshootLeft={false}
                 containerStyle={styles.swipeableContainer}
                 renderLeftActions={() => (
                   <Pressable
-                    onPress={() => handleRemove(item.id)}
+                    onPress={() => handleRemove(item.id, index)}
                     style={styles.swipeableRemove}
                   >
                     <Trash size={32} color={THEME.COLORS.GREY_100} />
