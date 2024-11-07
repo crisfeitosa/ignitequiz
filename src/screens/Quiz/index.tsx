96b3a5e7fb725827bc5f4399d2fea8b41c72b436
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-11-07 09:03:37 -0300

    Animando o OverlayFeedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -44,6 +44,7 @@ export function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+  const [statusReply, setStatusReply] = useState(0);
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
@@ -90,13 +91,16 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints(prevState => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
-      handleNextQuestion();
     }
 
     setAlternativeSelected(null);
+
+    handleNextQuestion();
   }
 
   function handleStop() {
@@ -209,7 +213,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <OverlayFeedback status={1} />
+      <OverlayFeedback status={statusReply} />
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>
           {quiz.title}
