52cb4791946ef76a2b0f5b2dbb323f0ca99858f0
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-11-07 10:12:50 -0300

    Animando com Skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
-
-import { styles } from './styles';
+import { useSharedValue, withTiming } from 'react-native-reanimated';
 import {
+  BlurMask,
   Canvas,
   Path,
-  Skia
+  Skia,
 } from '@shopify/react-native-skia';
+
+import { styles } from './styles';
 import { THEME } from '../../styles/theme';
 
 type Props = TouchableOpacityProps & {
@@ -17,10 +20,20 @@ const CHECK_SIZE = 28;
 const CHECK_STROKE = 2;
 
 export function Option({ checked, title, ...rest }: Props) {
+  const percentage = useSharedValue(0);
+
   const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
 
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS);
+
+  useEffect(() => {
+    if(checked) {
+      percentage.value = withTiming(1, { duration: 700});
+    } else {
+      percentage.value = withTiming(0, { duration: 700});
+    }
+  }, [checked]);
 
   return (
     <TouchableOpacity
@@ -44,14 +57,16 @@ export function Option({ checked, title, ...rest }: Props) {
           strokeWidth={CHECK_STROKE}
         />
 
-        <Path
+        <Path 
           path={path}
           color={THEME.COLORS.BRAND_LIGHT}
-          style='stroke'
+          style="stroke"
           strokeWidth={CHECK_STROKE}
           start={0}
-          end={0.5}
-        />
+          end={percentage}
+        >
+          <BlurMask blur={1} style="solid" />
+        </Path>
       </Canvas>
     </TouchableOpacity>
   );
